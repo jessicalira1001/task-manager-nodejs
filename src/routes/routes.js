a48cbec8b9d916c6670c5b9ecdb25e8ee23fbb2c
jessicalira1001 <jessicalira1001@gmail.com>
2025-06-28 21:00:22 -0300

    feat: adicionar rota para listar tarefas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const listarTarefas = require("../controllers/taskController")
 
 const rotas = express.Router();
 
-rotas.post("/task");
-rotas.put("/task/:id?");
-rotas.get("/task");
-rotas.get("/task/:id?")
-rotas.delete("/task/:id?")
+//rotas.post("/task");
+//rotas.put("/task/:id?");
+rotas.get("/task", listarTarefas);
+//rotas.get("/task/:id?")
+//rotas.delete("/task/:id?")
 
 
 module.exports = rotas;
