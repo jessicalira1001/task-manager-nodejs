c23f43f990ffd2609825ab9f62f1d05e8f68cd04
jessicalira1001 <jessicalira1001@gmail.com>
2025-07-02 20:15:57 -0300

    cleanup: Remove console.logs e trechos de codigo não utilizados

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,48 +1,43 @@
 const tarefasTeste = require("../bdTeste")
-//const client = require('../config/conexao.js')
 const {insertTask, selectTasks, updateTask, selectTaskId, deleteTask } = require('../models/taskModel.js')
-//const = require('../models/taskModel.js')
+
 
 const listarTarefas = async (req, res) => {
+
      const listaDeTasks = await selectTasks();
-     console.log("Aqui é controller")
-     console.log(listaDeTasks)
      res.status(200).json(listaDeTasks)
 }
 
 const criarTarefa = async (req, res) => {
-    console.log("Passei aqui no controler antes de chamar a insertTask")
+    
     const {id, descricao} = req.body;
-    console.log(id)
-    console.log(descricao)
+    
     const newTask = await insertTask(id, descricao);
-    console.log(newTask)
     res.status(201).json(newTask);
 }
 
 const atualizarTarefa = async (req, res) => {
-    const {id} = req.params;
 
+    const {id} = req.params;
     const {descricao} = req.body;
 
     const taskAtualizada = await updateTask(descricao, id);
     res.status(201).json(taskAtualizada);
-    
 }
 
 const acharTarefa = async (req, res) => {
+
     const {id} = req.params;
 
-    const taskEncontrada = await selectTaskId(id);
-        
+    const taskEncontrada = await selectTaskId(id);    
     res.status(200).json(taskEncontrada);
 }
 
 const excluirTarefa = async (req, res) => {
+
     const {id} = req.params;
 
     const taskExcluida = await deleteTask(id);
-
     res.status(200).json(taskExcluida);
 }
 
--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -1,24 +1,15 @@
 const pool = require('../config/conexao.js')
 
-
 const insertTask = async (id, descricao) => {
     
-
     const result = await pool.query('INSERT INTO tasks (id, descricao) VALUES ($1, $2) RETURNING *', [id, descricao]);
-    console.log("Passei aqui depois de chamar insertTask")
-    console.log(result)
     return result.rows[0];
-
-   
 }
 
 const selectTasks = async () => {
 
     const result = await pool.query('SELECT * FROM tasks');
-    console.log("Aqui é model")
-    console.log(result.rows)
     return result.rows;
-
 }
 
 const updateTask = async (descricao, id) => {
