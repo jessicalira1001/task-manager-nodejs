fc03f235d698159a2dc1ffaed092b9a22f9ac9fa
jessicalira1001 <jessicalira1001@gmail.com>
2025-07-04 20:37:48 -0300

    refactor: finalizando a migracao para o padrao MSCR

--- src/repositories/taskRepository.js
+++ src/repositories/taskRepository.js
@@ -1,29 +1,30 @@
 const pool = require('../models/db.js')
 
+
+const selectTasks = async () => {
+    console.log("Passei em repository")
+    const result = await pool.query('SELECT * FROM tasks');
+    return result.rows;
+}
+
 const insertTask = async (id, descricao) => {
     
     const result = await pool.query('INSERT INTO tasks (id, descricao) VALUES ($1, $2) RETURNING *', [id, descricao]);
     return result.rows[0];
 }
 
-const selectTasks = async () => {
-
-    const result = await pool.query('SELECT * FROM tasks');
-    return result.rows;
-}
-
-const updateTask = async (descricao, id) => {
+const updateTask = async ( descricao, id) => {
     
-    const result = await pool.query('UPDATE tasks SET descricao = $1 WHERE id = $2', [descricao, id])
+    const result = await pool.query('UPDATE tasks SET descricao = $1 WHERE id = $2', [descricao, parseInt(id)])
     return result.rows[0];
 }
 
-const selectTaskId = async (id) => {
+const selectTaskById = async (id) => {
     const result = await pool.query('SELECT * FROM tasks WHERE id = $1', [id]);
     return result.rows[0]
 }
 
-const deleteTask = async (id) => {
+const deleteTaskById = async (id) => {
     const result = await pool.query('DELETE FROM tasks WHERE id = $1', [id])
     return result.rows[0]
 }
@@ -32,6 +33,6 @@ module.exports = {
     insertTask,
     selectTasks,
     updateTask,
-    selectTaskId,
-    deleteTask
+    selectTaskById,
+    deleteTaskById
 };
