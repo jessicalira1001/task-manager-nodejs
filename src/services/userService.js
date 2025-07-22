2a9abb40e68eeae854aefd9e3a37362ed151d4fd
jessicalira1001 <jessicalira1001@gmail.com>
2025-07-22 09:10:41 -0300

    feat: adicionar criptografia de senha com bcrypt

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,12 +1,15 @@
 const {insertUser} = require('../repositories/userRepository.js')
+const bcrypt = require('bcrypt')
 
 const createUser = async (nome, email, senha) => {
     console.log(nome)
     if(!nome || nome === "" || !email || email === "" || !senha || senha === ""){
         throw new Error('É necessário um ID, um nome, um email e uma senha para criar um usuário');
     }
-    const status = "pendente";
-    return await insertUser(nome, email, senha);
+    
+    const senhaCriptograda = await bcrypt.hash(senha, 10)
+
+    return await insertUser(nome, email, senhaCriptograda);
 }
 
 
