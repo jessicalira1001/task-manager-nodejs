6fc7140be9ce1d24c019ce33db2e481d4111fd5a
jessicalira1001 <jessicalira1001@gmail.com>
2025-07-29 21:02:09 -0300

    feat: Adiciona a criação do schema para validação de dados de login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,13 @@
 const {login} = require('../services/authService.js')
+const {authSchema} = require('../schemas/authSchema.js')
 
 const postLogin = async (req, res) => {
     try {
+        const {error} = authSchema.validate(req.body);
+        if(error){
+            return res.status(400).json({mensagem: error.details[0].message});
+        }
+
         const {email, senha} = req.body;
         const newLogin = await login(email, senha);
         res.status(200).json(newLogin);
