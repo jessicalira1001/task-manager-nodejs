0388dbe45e3af8c0535949d1e524a748e3384bb9
jessicalira1001 <jessicalira1001@gmail.com>
2025-07-30 21:16:30 -0300

    feat: Adiciona a criação do schema para validação de dados de task

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { postTask, getTasks, putTask, getTaskById, deleteTask} = require("../controllers/taskController")
 const checkUserLoggedIn = require("../middlewares/authMiddleware")
+const validateSchema = require('../middlewares/validateSchema')
+const {taskSchema, updateTaskSchema} = require('../schemas/taskSchema');
 
 const rotasTask = express.Router();
 
-rotasTask.post("/task", checkUserLoggedIn, postTask);
-rotasTask.put("/task/:id", checkUserLoggedIn, putTask);
+rotasTask.post("/task", checkUserLoggedIn, validateSchema(taskSchema), postTask);
+rotasTask.put("/task/:id", checkUserLoggedIn, validateSchema(updateTaskSchema), putTask);
 rotasTask.get("/task", checkUserLoggedIn, getTasks);
 rotasTask.get("/task/:id", checkUserLoggedIn, getTaskById)
 rotasTask.delete("/task/:id", checkUserLoggedIn, deleteTask)
