d78c220c1abebd63be7b90e2cb0107278ce1b504
jessicalira1001 <jessicalira1001@gmail.com>
2025-07-31 20:06:24 -0300

    fix: Adiciona a verificação de email já cadastrado em createUser

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,13 @@
 const bcrypt = require('bcrypt')
 const ApiError = require('../utils/ApiError.js')
-const {insertUser} = require('../repositories/userRepository.js')
+const {insertUser, selectUserByEmail} = require('../repositories/userRepository.js')
 
 
 const createUser = async (nome, email, senha) => {
+    const emailJaCadastrado = await selectUserByEmail(email);
+    if(emailJaCadastrado.rows.length > 0){
+        throw new ApiError(400, 'Já existe um usuário com esse email cadastrado');
+    }
 
     const senhaCriptografada = await bcrypt.hash(senha, 10)
     const newUser = await insertUser(nome, email, senhaCriptografada);
